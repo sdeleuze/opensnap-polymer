509c22ff2f69c21b6fd7165ecac26e4895680161
Sebastien Deleuze <sdeleuze@pivotal.io>
2014-11-01 19:38:02 +0100

    Use core-animated-pages

--- os-client/lib/components/app_element.dart
+++ os-client/lib/components/app_element.dart
@@ -8,10 +8,18 @@ import '../os_client.dart';
 @CustomTag('app-element')
 class AppElement extends PolymerElement {
   
-  @observable final ObservableList<User> users = new ObservableList<User>();
-  @observable final ObservableList<Snap> snapSent = new ObservableList<Snap>();
-  @observable final ObservableList<Snap> snapReceived = new ObservableList<Snap>();
-  @observable String visibleContent = 'Photo';
+  static const _PHOTO = 0;
+  static const _RECEIVED = 1;
+  static const _SENT = 2;
+  
+  @observable
+  final ObservableList<User> users = new ObservableList<User>();
+  @observable
+  final ObservableList<Snap> snapSent = new ObservableList<Snap>();
+  @observable
+  final ObservableList<Snap> snapReceived = new ObservableList<Snap>();
+  @observable
+  int selected = _PHOTO;
   
   UserSync _userSync = new UserSync();
   SnapSync _snapSync = new SnapSync();
@@ -24,13 +32,13 @@ class AppElement extends PolymerElement {
       User currentUser = users.first;
       _snapSync.getSent(currentUser).then((_) => snapSent.addAll(_));
       _snapSync.getReceived(currentUser).then((_) => snapReceived.addAll(_));
-      _stompListener.subscribeSnapInbox(currentUser, onCreated);
+      _stompListener.subscribeSnapInbox(currentUser, onReceived);
     });
   }
   
-  void onCreated(Snap snap) {
+  void onReceived(Snap snap) {
+    //this.$['snapReceivedToast'].show();
     snapReceived.add(snap);
-    this.$['snapReceivedToast'].show();
   }
   
   void toggleMenu() {
@@ -38,25 +46,12 @@ class AppElement extends PolymerElement {
     drawerPanel.togglePanel();
   }
   
-  int selectedMenuItem(String content) {
-    switch(content) {
-      case 'Photo': return 0;
-      case 'Received': return 1;
-      case 'Sent': return 2;
-    }
-    return -1;
-  }
-  
-  void onMenuItemClick(event, details, target) {
-    visibleContent = target.label;
-  }
-  
   void sendSnap(event, details, target) {
-    //this.$['sendSnapStartToast'].show();
+//    this.$['sendSnapStartToast'].show();
     Snap snap = details;
     _snapSync.send(snap).then((_) {
       snapSent.add(_);
-      visibleContent = 'Sent';
+      selected = _SENT;
       this.$['sendSnapCompletedToast'].show();
       this.$['photoElement'].reset();
     });
